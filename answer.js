00a15f2569f0056c7cd38d521d872d464f0a6919
Amanda Nunn <amandanunn@thearrayschool.com>
2018-12-13 15:09:53 -0700

    final solution

--- answer.js
+++ answer.js
@@ -1,1 +1,11 @@
 // *** YOUR ANSWER BELOW ***
+function countdown(n){
+  str = "The world is going to end in: ";
+
+  while (n >= 0){
+    str += `${n}, `;
+    n--;
+  }
+  return str;
+}
+countdown(10);
